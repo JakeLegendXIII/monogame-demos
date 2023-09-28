4aff41d87ce1bfa39af35ceb651589697ba467a1
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-09-27 20:19:18 -0400

    Starting to add particle systems

---
+++ Chopper/Chopper/Engine/Particles/RandomNumberGenerator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chopper.Engine.Particles
+{
+    public class RandomNumberGenerator
+    {
+        private Random _random;
+
+        public RandomNumberGenerator()
+        {
+            _random = new Random();
+        }
+
+        public int NextRandom() => _random.Next();
+        public int NextRandom(int maxValue) => _random.Next(maxValue);
+        public int NextRandom(int minValue, int maxValue) => _random.Next(minValue, maxValue);
+
+        public float NextRandom(float maxValue) => (float)_random.NextDouble() * maxValue;
+        public float NextRandom(float minValue, float maxValue) => (float)_random.NextDouble() * (maxValue - minValue) + minValue;
+    }
+}
