0796524fb96c56945de253a07189186e65ee0e64
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-10-01 12:08:06 -0400

    Add Bounding boxes for AABB collision detection

--- Chopper/Chopper/Engine/Objects/BaseGameObject.cs
+++ Chopper/Chopper/Engine/Objects/BaseGameObject.cs
@@ -2,14 +2,18 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using System;
+using System.Collections.Generic;
 
 namespace Chopper.Engine.Objects
 {
     public class BaseGameObject
     {
         protected Texture2D _texture;
+        protected Texture2D _boundingBoxTexture;
 
-        protected Vector2 _position;
+        protected Vector2 _position = Vector2.One;
+        protected List<BoundingBox> _boundingBoxes = new List<BoundingBox>();
+
         public int zIndex;
         public event EventHandler<BaseGameStateEvent> OnObjectChanged;
 
@@ -22,11 +26,29 @@ namespace Chopper.Engine.Objects
         public int Height
         {
             get { return _texture.Height; }
-        }   
+        }
         public virtual Vector2 Position
         {
             get { return _position; }
-            set { _position = value; }
+            set
+            {
+                var deltaX = value.X - _position.X;
+                var deltaY = value.Y - _position.Y;
+                _position = value;
+
+                foreach (var bb in _boundingBoxes)
+                {
+                    bb.Position = new Vector2(bb.Position.X + deltaX, bb.Position.Y + deltaY);
+                }
+            }
+        }
+
+        public List<BoundingBox> BoundingBoxes
+        {
+            get
+            {
+                return _boundingBoxes;
+            }
         }
 
         public virtual void OnNotify(BaseGameStateEvent eventType)
@@ -39,9 +61,33 @@ namespace Chopper.Engine.Objects
             spriteBatch.Draw(_texture, _position, Color.White);
         }
 
+        public void RenderBoundingBoxes(SpriteBatch spriteBatch)
+        {
+            if (_boundingBoxTexture == null)
+            {
+                CreateBoundingBoxTexture(spriteBatch.GraphicsDevice);
+            }
+
+            foreach (var bb in _boundingBoxes)
+            {
+                spriteBatch.Draw(_boundingBoxTexture, bb.Rectangle, Color.Red);
+            }
+        }
+
+        private void CreateBoundingBoxTexture(GraphicsDevice graphicsDevice)
+        {
+            _boundingBoxTexture = new Texture2D(graphicsDevice, 1, 1);
+            _boundingBoxTexture.SetData<Color>(new Color[] { Color.White });
+        }
+
         public void SendEvent(BaseGameStateEvent e)
         {
             OnObjectChanged?.Invoke(this, e);
+        }
+
+        public void AddBoundingBox(BoundingBox bb)
+        {
+            _boundingBoxes.Add(bb);
         }
 
         public void Destroy()
--- Chopper/Chopper/Program.cs
+++ Chopper/Chopper/Program.cs
@@ -5,9 +5,9 @@ using Chopper.States.Dev;
 const int WIDTH = 1280;
 const int HEIGHT = 720;
 
-//using var game = new MainGame(WIDTH, HEIGHT, new SplashState());
+using var game = new MainGame(WIDTH, HEIGHT, new SplashState());
 // used to run in non-game mode so you can test out things like particles
-using var game = new MainGame(WIDTH, HEIGHT, new DevState());
+// using var game = new MainGame(WIDTH, HEIGHT, new DevState());
 game.Run();
 
 
