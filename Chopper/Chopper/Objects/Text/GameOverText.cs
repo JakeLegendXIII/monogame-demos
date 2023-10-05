4601fc7df71d1e50b64860af943cb532161cc6cd
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-10-04 21:03:34 -0400

    Added Text objects

---
+++ Chopper/Chopper/Objects/Text/GameOverText.cs
@@ -0,0 +1,14 @@
+﻿using Chopper.Engine.Objects;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Chopper.Objects.Text
+{
+    public class GameOverText : BaseTextObject
+    {
+        public GameOverText(SpriteFont font)
+        {
+            _font = font;
+            Text = "Game Over";           
+        }
+    }
+}
---
+++ Chopper/Chopper/Objects/Text/LivesText.cs
@@ -0,0 +1,28 @@
+﻿using Chopper.Engine.Objects;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Chopper.Objects.Text
+{
+    public class LivesText : BaseTextObject
+    {
+        private int _nbLives = -1;
+
+        public int NbLives
+        {
+            get
+            {
+                return _nbLives;
+            }
+            set
+            {
+                _nbLives = value;
+                Text = $"Lives: {_nbLives}";
+            }
+        }
+
+        public LivesText(SpriteFont font)
+        {
+            _font = font;
+        }
+    }
+}
