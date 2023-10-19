af8bf64d987f4ee2d276ec7d19ee1517b46f1da4
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-10-18 20:48:09 -0400

    Add files and create LevelImporter

---
+++ Chopper/ChopperPipelineExtensions/LevelImporter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+using System.IO;
+
+namespace ChopperPipelineExtensions
+{
+    [ContentImporter(".txt", DisplayName = "LevelImporter", DefaultProcessor = "LevelProcessor")]
+    public class LevelImporter : ContentImporter<string>
+    {
+        public override string Import(string fileName, ContentImporterContext context)
+        {
+            return File.ReadAllText(fileName);
+        }
+    }
+}
