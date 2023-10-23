57eabe4b643cbfab62e335caa61fcb124682f23f
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-10-22 23:49:30 -0400

    Added Engine2DPipelineExtensions project and AnimationData schema

---
+++ Chopper/Engine2DPipelineExtensions/AnimationData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Engine2DPipelineExtensions
+{
+    public class AnimationData
+    {
+        public int AnimationSpeed { get; set; }
+        public bool IsLooping { get; set; }
+        public List<AnimationDataFrame> Frames { get; set; }
+    }
+
+    public class AnimationDataFrame
+    {
+        public int X;
+        public int Y;
+        public int CellWidth;
+        public int CellHeight;
+    }
+}
