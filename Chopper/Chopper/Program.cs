855ae44a66c76bd39d33e9606d71c46f0fb5fa50
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-10-27 22:16:50 -0400

    Japanese is working now

--- Chopper/Chopper/Program.cs
+++ Chopper/Chopper/Program.cs
@@ -8,8 +8,8 @@ using System.Globalization;
 const int WIDTH = 1280;
 const int HEIGHT = 720;
 
-Strings.Culture = CultureInfo.CurrentCulture;
-// Strings.Culture = CultureInfo.GetCultureInfo("ja-JP");
+//Strings.Culture = CultureInfo.CurrentCulture;
+Strings.Culture = CultureInfo.GetCultureInfo("ja-JP");
 // Strings.Culture = CultureInfo.GetCultureInfo("fr-FR");
 
 using (var game = new MainGame(WIDTH, HEIGHT, new SplashState(), true))
