402b7a1199a8a5ee12a9d928960d5b5907f4b4dd
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-10-31 20:03:40 -0400

    Working on dev TestCameraState setup

--- Chopper/Chopper/States/Dev/TestCameraState.cs
+++ Chopper/Chopper/States/Dev/TestCameraState.cs
@@ -1,6 +1,7 @@
 ﻿using Chopper.Engine.States;
 using Chopper.Objects;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using MonoGame.Extended;
 using MonoGame.Extended.ViewportAdapters;
 
@@ -12,12 +13,27 @@ namespace Chopper.States.Dev
 		private const string PlayerAnimationTurnLeft = "Sprites/Animations/turn_left";
 		private const string PlayerAnimationTurnRight = "Sprites/Animations/turn_right";
 		private PlayerSprite _playerSprite;
+
 		private OrthographicCamera _camera;
+
+		private const float CAMERA_SPEED = 10.0f;
 
 		public override void LoadContent()
 		{
 			var viewportAdapter = new DefaultViewportAdapter(_graphicsDevice);
 			_camera = new OrthographicCamera(viewportAdapter);
+			_playerSprite = new PlayerSprite(LoadTexture(PlayerFighter), 
+				LoadAnimation(PlayerAnimationTurnLeft), LoadAnimation(PlayerAnimationTurnRight));
+			_playerSprite.Position = new Vector2(0, 0);
+		}
+
+		public override void Render(SpriteBatch spriteBatch)
+		{
+			var transformMatrix = _camera.GetViewMatrix();
+
+			spriteBatch.Begin(transformMatrix: transformMatrix);
+				_playerSprite.Render(spriteBatch);
+			spriteBatch.End();			
 		}
 
 		public override void HandleInput(GameTime gameTime)
@@ -27,7 +43,8 @@ namespace Chopper.States.Dev
 
 		public override void UpdateGameState(GameTime gameTime)
 		{
-			throw new System.NotImplementedException();
+			_playerSprite.Update(gameTime);
+			_camera.Position += new Vector2(0, -CAMERA_SPEED);
 		}
 
 		protected override void SetInputManager()
