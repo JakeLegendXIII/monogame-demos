21e607898b80c63ee9a91c6a2255324bcbd68de2
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-11-02 20:27:20 -0400

    Editor is showing Cornflower Blue screen now :)

--- Chopper/GameEditor/GameControl.cs
+++ Chopper/GameEditor/GameControl.cs
@@ -7,17 +7,15 @@ namespace GameEditor
 	{		
 		protected override void Initialize()
 		{
-			Initialize();
+			
 		}
 
 		protected override void Update(GameTime gameTime)
-		{
-			Update(gameTime);
+		{			
 		}
 
 		protected override void Draw()
-		{
-			Draw();
+		{			
 		}
 	}
 }
--- Chopper/GameEditor/GameEditorForm.Designer.cs
+++ Chopper/GameEditor/GameEditorForm.Designer.cs
@@ -28,12 +28,31 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			components = new System.ComponentModel.Container();
+			gameControl1 = new GameControl();
+			SuspendLayout();
+			// 
+			// gameControl1
+			// 
+			gameControl1.Location = new Point(81, 12);
+			gameControl1.MouseHoverUpdatesOnly = false;
+			gameControl1.Name = "gameControl1";
+			gameControl1.Size = new Size(643, 375);
+			gameControl1.TabIndex = 0;
+			gameControl1.Text = "gameControl1";
+			// 
+			// GameEditorForm
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(800, 450);
+			Controls.Add(gameControl1);
+			Name = "GameEditorForm";
 			Text = "Form1";
+			ResumeLayout(false);
 		}
 
 		#endregion
+
+		private GameControl gameControl1;
 	}
 }
