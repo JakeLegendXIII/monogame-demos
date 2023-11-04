4f642efaa4d699d5347947e2070c15bd79161bd2
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-11-04 02:45:28 -0400

    Cleanup

--- Chopper/Chopper/States/Dev/DevInputCommand.cs
+++ Chopper/Chopper/States/Dev/DevInputCommand.cs
@@ -10,7 +10,6 @@ namespace Chopper.States.Dev
     public class DevInputCommand : BaseInputCommand
     {
 		// Out of Game Commands
-		// Out of Game Commands
 		public class DevQuit : DevInputCommand { }
 		public class DevExplode : DevInputCommand { }
 		public class DevMissileExplode : DevInputCommand { }
