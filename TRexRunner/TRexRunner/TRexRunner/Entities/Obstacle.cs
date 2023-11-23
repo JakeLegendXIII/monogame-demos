761eb970e6d61698de7baa05b52fccc67a166ed6
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-11-22 23:16:32 -0500

    Small tweak to Obstacles

--- TRexRunner/TRexRunner/TRexRunner/Entities/Obstacle.cs
+++ TRexRunner/TRexRunner/TRexRunner/Entities/Obstacle.cs
@@ -7,7 +7,6 @@ namespace TRexRunner.Entities
 	public abstract class Obstacle : IGameEntity
 	{
 		private Trex _trex;
-		protected Sprite _sprite;
 
 		public Vector2 Position { get; private set; }
 
@@ -15,20 +14,16 @@ namespace TRexRunner.Entities
 
 		public abstract Rectangle CollisionBox { get; }
 
-		protected Obstacle(Trex trex, Vector2 position, Sprite sprite)
+		protected Obstacle(Trex trex, Vector2 position)
 		{
 			_trex = trex;
 			Position = position;
-			_sprite = sprite;
 		}
 
-		public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
-		{
-
-		}
+		public abstract void Draw(SpriteBatch spriteBatch, GameTime gameTime);
 
 		public void Update(GameTime gameTime)
-		{			
+		{
 			Position = new Vector2(Position.X - _trex.Speed * (float)gameTime.ElapsedGameTime.TotalSeconds, Position.Y);
 		}
 	}
