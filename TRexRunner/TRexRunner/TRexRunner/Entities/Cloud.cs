7153e787146aed5b2a03a0bba23059efd395eab5
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-11-26 03:57:12 -0500

    Starting to setup sky and create clouds

---
+++ TRexRunner/TRexRunner/TRexRunner/Entities/Cloud.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using TRexRunner.Graphics;
+
+namespace TRexRunner.Entities
+{
+	public class Cloud : SkyObject
+	{
+		private const int TEXTURE_COORDS_X = 67;
+		private const int TEXTURE_COORDS_Y = 0;
+
+		private const int TEXTURE_WIDTH = 46;
+		private const int TEXTURE_HEIGHT = 17;
+
+		private Sprite _sprite;
+
+		public override float Speed => _trex.Speed * 0.5f;
+
+		public Cloud(Trex trex, Vector2 position, Texture2D spriteSheet) : base(trex, position)
+		{
+			_sprite = new Sprite(spriteSheet, TEXTURE_COORDS_X, TEXTURE_COORDS_Y, TEXTURE_WIDTH, TEXTURE_HEIGHT);
+		}		
+
+		public override void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+		{
+			_sprite.Draw(spriteBatch, Position);
+		}
+	}
+}
---
+++ TRexRunner/TRexRunner/TRexRunner/Entities/SkyManager.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TRexRunner.Entities
+{
+	public class SkyManager : IGameEntity
+	{
+		public int DrawOrder => 0;
+
+		private EntityManager _entityManager;
+
+		public SkyManager(EntityManager entityManager)
+		{
+			_entityManager = entityManager;
+		}
+
+		public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
+		{
+			
+		}
+
+		public void Update(GameTime gameTime)
+		{
+			
+		}
+	}
+}
