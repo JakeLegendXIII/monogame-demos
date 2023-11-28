dc636cab7a2560baae31fb116145510f9b3008e9
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-11-27 20:49:05 -0500

    Stars are spawning

--- TRexRunner/TRexRunner/TRexRunner/Entities/SkyManager.cs
+++ TRexRunner/TRexRunner/TRexRunner/Entities/SkyManager.cs
@@ -15,6 +15,12 @@ namespace TRexRunner.Entities
 		private const int CLOUD_MIN_DISTANCE = 150;
 		private const int CLOUD_MAX_DISTANCE = 400;
 
+		private const int STAR_MIN_POS_X = 10;
+		private const int STAR_MAX_POS_Y = 60;
+
+		private const int STAR_MIN_DISTANCE = 120;
+		private const int STAR_MAX_DISTANCE = 380;
+
 		public int DrawOrder => 0;
 
 		private EntityManager _entityManager;
@@ -23,6 +29,7 @@ namespace TRexRunner.Entities
 		private double _lastCloudSpawnScore = -1;
 
 		private int _targetCloudDistance = -1;
+		private int _targetStarDistance = -1;
 		private Random _random;
 		private Texture2D _spriteSheet;
 		private Trex _trex;
@@ -43,6 +50,33 @@ namespace TRexRunner.Entities
 
 		public void Update(GameTime gameTime)
 		{
+			HandleCloudSpawning();
+
+			HandleStarSpawning();
+
+			foreach (var so in _entityManager.GetEntitiesOfType<SkyObject>().Where(c => c.Position.X < -200))
+			{
+				_entityManager.RemoveEntity(so);
+			}
+		}
+
+		private void HandleStarSpawning()
+		{
+			IEnumerable<Star> stars = _entityManager.GetEntitiesOfType<Star>();
+
+			if (stars.Count() <= 0 || (MainGame.WINDOW_WIDTH - stars.Max(c => c.Position.X) >= _targetStarDistance))
+			{
+				_targetStarDistance = _random.Next(STAR_MIN_DISTANCE, STAR_MAX_DISTANCE + 1);
+				int posY = _random.Next(STAR_MIN_POS_X, STAR_MAX_POS_Y + 1);
+
+				Star star = new Star(_trex, new Vector2(MainGame.WINDOW_WIDTH, posY), _spriteSheet);
+
+				_entityManager.AddEntity(star);
+			}
+		}
+
+		private void HandleCloudSpawning()
+		{
 			IEnumerable<Cloud> clouds = _entityManager.GetEntitiesOfType<Cloud>();
 
 			if (clouds.Count() <= 0 || (MainGame.WINDOW_WIDTH - clouds.Max(c => c.Position.X) >= _targetCloudDistance))
@@ -53,11 +87,6 @@ namespace TRexRunner.Entities
 				Cloud cloud = new Cloud(_trex, new Vector2(MainGame.WINDOW_WIDTH, posY), _spriteSheet);
 
 				_entityManager.AddEntity(cloud);
-			}
-
-			foreach (var cl in clouds.Where(c => c.Position.X < -200))
-			{
-				_entityManager.RemoveEntity(cl);
 			}
 		}
 	}
