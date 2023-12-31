7544b315704894a2d4b8b116d637b10491b67528
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-12-30 23:43:46 -0500

    More cleanup

--- Chopper/Chopper/Engine/Sound/SoundAttributes.cs
+++ Chopper/Chopper/Engine/Sound/SoundAttributes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Chopper.Engine.Sound
+﻿namespace Chopper.Engine.Sound
 {
     public class SoundAttributes
     {
--- Chopper/Chopper/Engine/Sound/SoundManager.cs
+++ Chopper/Chopper/Engine/Sound/SoundManager.cs
@@ -2,9 +2,6 @@
 using Microsoft.Xna.Framework.Audio;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Chopper.Engine.Sound
 {
