68bf4ad1ec1f2518906017e756192eef726242e9
Jacob Kirkbride <jakelegendxiii@gmail.com>
2023-12-30 23:32:37 -0500

    Couple more small using cleanups

--- Chopper/Chopper/Engine/Input/InputManager.cs
+++ Chopper/Chopper/Engine/Input/InputManager.cs
@@ -1,10 +1,6 @@
 ﻿using Chopper.Engine.Input;
 using Microsoft.Xna.Framework.Input;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Chopper.Engine.Input
 {
