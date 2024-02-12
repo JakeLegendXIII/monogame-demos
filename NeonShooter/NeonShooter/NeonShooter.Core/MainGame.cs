775cbb04d02744b86f7ef797b1fd75d788a5a976
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-02-11 22:25:04 -0500

    Setup basic project structure

--- NeonShooter/NeonShooter/NeonShooter.Core/MainGame.cs
+++ NeonShooter/NeonShooter/NeonShooter.Core/MainGame.cs
@@ -2,14 +2,14 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace NeonShooter
+namespace NeonShooter.Core
 {
-	public class Game1 : Game
+	public class MainGame : Game
 	{
 		private GraphicsDeviceManager _graphics;
 		private SpriteBatch _spriteBatch;
 
-		public Game1()
+		public MainGame()
 		{
 			_graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
--- NeonShooter/NeonShooter/NeonShooter/Program.cs
+++ NeonShooter/NeonShooter/NeonShooter/Program.cs
@@ -1,3 +1,3 @@
 ﻿
-using var game = new NeonShooter.Game1();
+using var game = new NeonShooter.Core.MainGame();
 game.Run();
