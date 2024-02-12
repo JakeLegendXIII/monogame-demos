29f9c850ff97af56cbc44fc1a50e04d490ea46ad
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-02-11 22:55:46 -0500

    Start adding entity code

---
+++ NeonShooter/NeonShooter/NeonShooter.Core/Entities/Entity.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NeonShooter.Core.Entities
+{
+	abstract class Entity
+	{
+		protected Texture2D image;
+		// The tint of the image. This will also allow us to change the transparency. 
+		protected Color color = Color.White;
+
+		public Vector2 Position, Velocity;
+		public float Orientation;
+		public float Radius = 20;   // used for circular collision detection 
+		public bool IsExpired;      // true if the entity was destroyed and should be deleted. 
+
+		public Vector2 Size
+		{
+			get
+			{
+				return image == null ? Vector2.Zero : new Vector2(image.Width, image.Height);
+			}
+		}
+
+		public abstract void Update();
+
+		public virtual void Draw(SpriteBatch spriteBatch)
+		{
+			spriteBatch.Draw(image, Position, null, color, Orientation, Size / 2f, 1f, 0, 0);
+		}
+	}
+}
---
+++ NeonShooter/NeonShooter/NeonShooter.Core/Entities/EntityManager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NeonShooter.Core.Entities
+{
+	static class EntityManager
+	{
+		static List<Entity> entities = new List<Entity>();
+		static bool isUpdating;
+		static List<Entity> addedEntities = new List<Entity>();
+		public static int Count { get { return entities.Count; } }
+
+		public static void Add(Entity entity)
+		{
+			if (!isUpdating)
+				entities.Add(entity);
+			else
+				addedEntities.Add(entity);
+		}
+		public static void Update()
+		{
+			isUpdating = true;
+			foreach (var entity in entities)
+				entity.Update();
+			isUpdating = false;
+			foreach (var entity in addedEntities)
+				entities.Add(entity);
+			addedEntities.Clear();
+			// remove any expired entities. 
+			entities = entities.Where(x => !x.IsExpired).ToList();
+		}
+		public static void Draw(SpriteBatch spriteBatch)
+		{
+			foreach (var entity in entities)
+				entity.Draw(spriteBatch);
+		}
+	}
+}
