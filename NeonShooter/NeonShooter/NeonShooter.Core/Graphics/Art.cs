71582d4f328d7989a67b630d75153586f4f6c957
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-02-11 23:06:01 -0500

    Add Art and get some sprites added

---
+++ NeonShooter/NeonShooter/NeonShooter.Core/Graphics/Art.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NeonShooter.Core.Graphics
+{
+	static class Art
+	{
+		public static Texture2D Player { get; private set; }
+		public static Texture2D Seeker { get; private set; }
+		public static Texture2D Wanderer { get; private set; }
+		public static Texture2D Bullet { get; private set; }
+		public static Texture2D Pointer { get; private set; }
+
+		public static Texture2D Pixel { get; private set; }     // a single white pixel
+
+		public static void Load(ContentManager content)
+		{
+			Player = content.Load<Texture2D>("Player");
+			Seeker = content.Load<Texture2D>("Seeker");
+			Wanderer = content.Load<Texture2D>("Wanderer");
+			Bullet = content.Load<Texture2D>("Bullet");
+			Pointer = content.Load<Texture2D>("Pointer");
+
+			Pixel = new Texture2D(Player.GraphicsDevice, 1, 1);
+			Pixel.SetData(new[] { Color.White });
+		}
+	}
+}
--- NeonShooter/NeonShooter/NeonShooter.Core/MainGame.cs
+++ NeonShooter/NeonShooter/NeonShooter.Core/MainGame.cs
@@ -4,13 +4,22 @@ using Microsoft.Xna.Framework.Input;
 
 namespace NeonShooter.Core
 {
+	/// <summary>
+	/// This is the main type for your game
+	/// </summary>
 	public class MainGame : Game
 	{
+		public static MainGame Instance { get; private set; }
+		public static Viewport Viewport { get { return Instance.GraphicsDevice.Viewport; } }
+		public static Vector2 ScreenSize { get { return new Vector2(Viewport.Width, Viewport.Height); } }
+
 		private GraphicsDeviceManager _graphics;
 		private SpriteBatch _spriteBatch;
 
 		public MainGame()
 		{
+			Instance = this;
+
 			_graphics = new GraphicsDeviceManager(this);
 			Content.RootDirectory = "Content";
 			IsMouseVisible = true;
