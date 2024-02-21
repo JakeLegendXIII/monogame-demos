fe25f42db6a4ffde6b3a67a3fc682c1639f27aeb
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-02-20 23:28:44 -0500

    Start particle system

---
+++ NeonShooter/NeonShooter/NeonShooter.Core/Graphics/ParticleManager.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace NeonShooter.Core.Graphics
+{
+	public class ParticleManager<T>
+	{
+		public class Particle
+		{
+			public Texture2D Texture;
+			public Vector2 Position;
+			public float Orientation;
+			public Vector2 Scale = Vector2.One;
+			public Color Color;
+			public float Duration;
+			public float PercentLife = 1f;
+			public T State;
+		}
+	}
+}
