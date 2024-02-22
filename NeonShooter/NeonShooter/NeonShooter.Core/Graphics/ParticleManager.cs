cf6770c8bd88448dfccb4aac7a2dfe13dfba01d4
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-02-21 21:43:13 -0500

    Building out particlemanager

--- NeonShooter/NeonShooter/NeonShooter.Core/Graphics/ParticleManager.cs
+++ NeonShooter/NeonShooter/NeonShooter.Core/Graphics/ParticleManager.cs
@@ -1,17 +1,66 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using System;
 
 namespace NeonShooter.Core.Graphics
 {
 	public class ParticleManager<T>
 	{
+		// This delegate will be called for each particle.
+		private Action<Particle> updateParticle;
+		private CircularParticleArray particleList;
+
+		/// <summary>
+		/// Allows creation of particles.
+		/// </summary>
+		/// <param name="capacity">The maximum number of particles. An array of this size will be pre-allocated.</param>
+		/// <param name="updateParticle">A delegate that lets you specify custom behaviour for your particles. Called once per particle, per frame.</param>
+		public ParticleManager(int capacity, Action<Particle> updateParticle)
+		{
+			this.updateParticle = updateParticle;
+			particleList = new CircularParticleArray(capacity);
+
+			// Populate the list with empty particle objects, for reuse.
+			for (int i = 0; i < capacity; i++)
+				particleList[i] = new Particle();
+		}
+
+		public void CreateParticle(Texture2D texture, Vector2 position, Color tint, float duration, Vector2 scale, T state, float theta = 0)
+		{
+			Particle particle;
+			if (particleList.Count == particleList.Capacity)
+			{
+				// if the list is full, overwrite the oldest particle, and rotate the circular list
+				particle = particleList[0];
+				particleList.Start++;
+			}
+			else
+			{
+				particle = particleList[particleList.Count];
+				particleList.Count++;
+			}
+
+			// Create the particle
+			particle.Texture = texture;
+			particle.Position = position;
+			particle.Tint = tint;
+
+			particle.Duration = duration;
+			particle.PercentLife = 1f;
+			particle.Scale = scale;
+			particle.Orientation = theta;
+			particle.State = state;
+		}
+
 		public class Particle
 		{
 			public Texture2D Texture;
 			public Vector2 Position;
 			public float Orientation;
+
 			public Vector2 Scale = Vector2.One;
-			public Color Color;
+
+			public Color Tint;
 			public float Duration;
 			public float PercentLife = 1f;
 			public T State;
