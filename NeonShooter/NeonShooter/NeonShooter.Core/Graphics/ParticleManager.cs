f5e5bcb658cfba0365c1638887520a3e20f3c853
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-02-21 22:01:09 -0500

    Add update and draw

--- NeonShooter/NeonShooter/NeonShooter.Core/Graphics/ParticleManager.cs
+++ NeonShooter/NeonShooter/NeonShooter.Core/Graphics/ParticleManager.cs
@@ -25,6 +25,51 @@ namespace NeonShooter.Core.Graphics
 				particleList[i] = new Particle();
 		}
 
+		/// <summary>
+		/// Update particle state, to be called every frame.
+		/// </summary>
+		public void Update()
+		{
+			int removalCount = 0;
+			for (int i = 0; i < particleList.Count; i++)
+			{
+				var particle = particleList[i];
+
+				updateParticle(particle);
+
+				particle.PercentLife -= 1f / particle.Duration;
+
+				// sift deleted particles to the end of the list
+				Swap(particleList, i - removalCount, i);
+
+				// if the alpha < 0, delete this particle
+				if (particle.PercentLife < 0)
+					removalCount++;
+			}
+			particleList.Count -= removalCount;
+		}
+
+		/// <summary>
+		/// Draw the particles.
+		/// </summary>
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			for (int i = 0; i < particleList.Count; i++)
+			{
+				var particle = particleList[i];
+
+				Vector2 origin = new Vector2(particle.Texture.Width / 2, particle.Texture.Height / 2);
+				spriteBatch.Draw(particle.Texture, particle.Position, null, particle.Tint, particle.Orientation, origin, particle.Scale, 0, 0);
+			}
+		}
+
+		private static void Swap(CircularParticleArray list, int index1, int index2)
+		{
+			var temp = list[index1];
+			list[index1] = list[index2];
+			list[index2] = temp;
+		}
+
 		public void CreateParticle(Texture2D texture, Vector2 position, Color tint, float duration, Vector2 scale, T state, float theta = 0)
 		{
 			Particle particle;
