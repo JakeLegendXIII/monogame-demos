6c087d31373d2a8a25e39ba14f733d34729597cc
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-02-21 21:30:09 -0500

    Add Circular Particle Array

--- NeonShooter/NeonShooter/NeonShooter.Core/Graphics/ParticleManager.cs
+++ NeonShooter/NeonShooter/NeonShooter.Core/Graphics/ParticleManager.cs
@@ -16,5 +16,34 @@ namespace NeonShooter.Core.Graphics
 			public float PercentLife = 1f;
 			public T State;
 		}
+
+		// Represents a circular array with an arbitrary starting point. It's useful for efficiently overwriting
+		// the oldest particles when the array gets full. Simply overwrite particleList[0] and advance Start.
+		private class CircularParticleArray
+		{
+			private int start;
+			public int Start
+			{
+				get { return start; }
+				set { start = value % list.Length; }
+			}
+
+			public int Count { get; set; }
+			public int Capacity { get { return list.Length; } }
+			private Particle[] list;
+
+			public CircularParticleArray() { }  // for serialization
+
+			public CircularParticleArray(int capacity)
+			{
+				list = new Particle[capacity];
+			}
+
+			public Particle this[int i]
+			{
+				get { return list[(start + i) % list.Length]; }
+				set { list[(start + i) % list.Length] = value; }
+			}
+		}
 	}
 }
