e83ed7bf7f4af8927100a55d321715f1ce4b182f
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-02-22 23:24:45 -0500

    Updating ParticleState

--- NeonShooter/NeonShooter/NeonShooter.Core/Graphics/ParticleState.cs
+++ NeonShooter/NeonShooter/NeonShooter.Core/Graphics/ParticleState.cs
@@ -1,12 +1,34 @@
 ﻿using Microsoft.Xna.Framework;
+using NeonShooter.Core.Utils;
+using System;
 
 namespace NeonShooter.Core.Graphics
 {
 	public enum ParticleType { None, Enemy, Bullet, IgnoreGravity }
+
 	public struct ParticleState
 	{
 		public Vector2 Velocity;
 		public ParticleType Type;
 		public float LengthMultiplier;
+
+		private static Random rand = new Random();
+
+		public ParticleState(Vector2 velocity, ParticleType type, float lengthMultiplier = 1f)
+		{
+			Velocity = velocity;
+			Type = type;
+			LengthMultiplier = lengthMultiplier;
+		}
+
+		public static ParticleState GetRandom(float minVel, float maxVel)
+		{
+			var state = new ParticleState();
+			state.Velocity = rand.NextVector2(minVel, maxVel);
+			state.Type = ParticleType.None;
+			state.LengthMultiplier = 1;
+
+			return state;
+		}
 	}
 }
--- NeonShooter/NeonShooter/NeonShooter.Core/Utils/Extensions.cs
+++ NeonShooter/NeonShooter/NeonShooter.Core/Utils/Extensions.cs
@@ -19,5 +19,12 @@ namespace NeonShooter.Core.Utils
 		{
 			return vector * (length / vector.Length());
 		}
+
+		public static Vector2 NextVector2(this Random rand, float minLength, float maxLength)
+		{
+			double theta = rand.NextDouble() * 2 * Math.PI;
+			float length = rand.NextFloat(minLength, maxLength);
+			return new Vector2(length * (float)Math.Cos(theta), length * (float)Math.Sin(theta));
+		}
 	}
 }
