4a31dddeb060996e05c8fd508caa50d9558a814e
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-02-23 20:38:25 -0500

    Make bullets explode and look cool

--- NeonShooter/NeonShooter/NeonShooter.Core/Entities/Bullet.cs
+++ NeonShooter/NeonShooter/NeonShooter.Core/Entities/Bullet.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.Xna.Framework;
 using NeonShooter.Core.Graphics;
 using NeonShooter.Core.Utils;
+using System;
 
 namespace NeonShooter.Core.Entities
 {
 	class Bullet : Entity
 	{
+		private static Random rand = new Random();
+
 		public Bullet(Vector2 position, Vector2 velocity)
 		{
 			image = Art.Bullet;
@@ -19,10 +22,20 @@ namespace NeonShooter.Core.Entities
 		{
 			if (Velocity.LengthSquared() > 0)
 				Orientation = Velocity.ToAngle();
+
 			Position += Velocity;
-			// delete bullets that go off-screen 
+			// MainGame.Grid.ApplyExplosiveForce(0.5f * Velocity.Length(), Position, 80);
+
+			// delete bullets that go off-screen
 			if (!MainGame.Viewport.Bounds.Contains(Position.ToPoint()))
+			{
 				IsExpired = true;
+
+				for (int i = 0; i < 30; i++)
+					MainGame.ParticleManager.CreateParticle(Art.LineParticle, Position, Color.LightBlue, 50, 1,
+						new ParticleState() { Velocity = rand.NextVector2(0, 9), Type = ParticleType.Bullet, LengthMultiplier = 1 });
+
+			}
 		}
 	}
 }
