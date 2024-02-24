9ec2d0c844ef080c9cfe225c3c2c1fbf5d0e50ca
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-02-24 14:24:09 -0500

    Add Grid and PointMass

---
+++ NeonShooter/NeonShooter/NeonShooter.Core/Entities/Grid.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace NeonShooter.Core.Entities
+{
+	public class Grid
+	{
+		private class PointMass
+		{
+			public Vector3 Position;
+			public Vector3 Velocity;
+			public float InverseMass;
+			private Vector3 acceleration;
+			private float damping = 0.98f;
+			public PointMass(Vector3 position, float invMass)
+			{
+				Position = position;
+				InverseMass = invMass;
+			}
+			public void ApplyForce(Vector3 force)
+			{
+				acceleration += force * InverseMass;
+			}
+			public void IncreaseDamping(float factor)
+			{
+				damping *= factor;
+			}
+			public void Update()
+			{
+				Velocity += acceleration;
+				Position += Velocity;
+				acceleration = Vector3.Zero;
+				if (Velocity.LengthSquared() < 0.001f * 0.001f)
+					Velocity = Vector3.Zero;
+				Velocity *= damping;
+				damping = 0.98f;
+			}
+		}
+	}
+}
