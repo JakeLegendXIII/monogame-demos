03eb9881bb9e78bb19e01ff68e871589246dcb67
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-02-23 20:58:29 -0500

    Improve BlackHole particles

--- NeonShooter/NeonShooter/NeonShooter.Core/Entities/BlackHole.cs
+++ NeonShooter/NeonShooter/NeonShooter.Core/Entities/BlackHole.cs
@@ -9,7 +9,9 @@ namespace NeonShooter.Core.Entities
 	class BlackHole : Entity
 	{
 		private static Random rand = new Random();
+
 		private int hitpoints = 10;
+		private float sprayAngle = 0;
 
 		public BlackHole(Vector2 position)
 		{
@@ -26,16 +28,39 @@ namespace NeonShooter.Core.Entities
 			{
 				if (entity is Enemy && !(entity as Enemy).IsActive)
 					continue;
-				// bullets are repelled by black holes and everything else is attracted 
+
+				// bullets are repelled by black holes and everything else is attracted
 				if (entity is Bullet)
 					entity.Velocity += (entity.Position - Position).ScaleTo(0.3f);
 				else
 				{
 					var dPos = Position - entity.Position;
 					var length = dPos.Length();
+
 					entity.Velocity += dPos.ScaleTo(MathHelper.Lerp(2, 0, length / 250f));
 				}
 			}
+
+			// The black holes spray some orbiting particles. The spray toggles on and off every quarter second.
+			if ((MainGame.GameTime.TotalGameTime.Milliseconds / 250) % 2 == 0)
+			{
+				Vector2 sprayVel = MathUtil.FromPolar(sprayAngle, rand.NextFloat(12, 15));
+				Color color = ColorUtil.HSVToColor(5, 0.5f, 0.8f);  // light purple
+				Vector2 pos = Position + 2f * new Vector2(sprayVel.Y, -sprayVel.X) + rand.NextVector2(4, 8);
+				var state = new ParticleState()
+				{
+					Velocity = sprayVel,
+					LengthMultiplier = 1,
+					Type = ParticleType.Enemy
+				};
+
+				MainGame.ParticleManager.CreateParticle(Art.LineParticle, pos, color, 190, 1.5f, state);
+			}
+
+			// rotate the spray direction
+			sprayAngle -= MathHelper.TwoPi / 50f;
+
+			// MainGame.Grid.ApplyImplosiveForce((float)Math.Sin(sprayAngle / 2) * 10 + 20, Position, 200);
 		}
 
 		public override void Draw(SpriteBatch spriteBatch)
@@ -49,7 +74,33 @@ namespace NeonShooter.Core.Entities
 		{
 			hitpoints--;
 			if (hitpoints <= 0)
+			{
 				IsExpired = true;
+				PlayerStatus.AddPoints(5);
+				PlayerStatus.IncreaseMultiplier();
+			}
+
+
+			float hue = (float)((3 * MainGame.GameTime.TotalGameTime.TotalSeconds) % 6);
+			Color color = ColorUtil.HSVToColor(hue, 0.25f, 1);
+			const int numParticles = 150;
+			float startOffset = rand.NextFloat(0, MathHelper.TwoPi / numParticles);
+
+			for (int i = 0; i < numParticles; i++)
+			{
+				Vector2 sprayVel = MathUtil.FromPolar(MathHelper.TwoPi * i / numParticles + startOffset, rand.NextFloat(8, 16));
+				Vector2 pos = Position + 2f * sprayVel;
+				var state = new ParticleState()
+				{
+					Velocity = sprayVel,
+					LengthMultiplier = 1,
+					Type = ParticleType.IgnoreGravity
+				};
+
+				MainGame.ParticleManager.CreateParticle(Art.LineParticle, pos, color, 90, 1.5f, state);
+			}
+
+			Sound.Sound.Explosion.Play(0.5f, rand.NextFloat(-0.2f, 0.2f), 0);
 		}
 
 		public void Kill()
