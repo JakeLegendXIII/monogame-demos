771847c9cfc585883d6a699280eec545b7fe6af5
Jacob Kirkbride <jakelegendxiii@gmail.com>
2024-02-25 18:37:39 -0500

    Add more to Grid

--- NeonShooter/NeonShooter/NeonShooter.Core/Entities/Grid.cs
+++ NeonShooter/NeonShooter/NeonShooter.Core/Entities/Grid.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using NeonShooter.Core.Utils;
+using System;
 using System.Collections.Generic;
 
 namespace NeonShooter.Core.Entities
@@ -132,5 +135,120 @@ namespace NeonShooter.Core.Entities
 				mass.Update();
 		}
 
+		public void Draw(SpriteBatch spriteBatch)
+		{
+			screenSize = MainGame.ScreenSize;
+
+			int width = points.GetLength(0);
+			int height = points.GetLength(1);
+			Color color = new Color(30, 30, 139, 85);   // dark blue
+
+			for (int y = 1; y < height; y++)
+			{
+				for (int x = 1; x < width; x++)
+				{
+					Vector2 left = new Vector2(), up = new Vector2();
+					Vector2 p = ToVec2(points[x, y].Position);
+					if (x > 1)
+					{
+						left = ToVec2(points[x - 1, y].Position);
+						float thickness = y % 3 == 1 ? 3f : 1f;
+
+						// use Catmull-Rom interpolation to help smooth bends in the grid
+						int clampedX = Math.Min(x + 1, width - 1);
+						Vector2 mid = Vector2.CatmullRom(ToVec2(points[x - 2, y].Position), left, p, ToVec2(points[clampedX, y].Position), 0.5f);
+
+						// If the grid is very straight here, draw a single straight line. Otherwise, draw lines to our
+						// new interpolated midpoint
+						if (Vector2.DistanceSquared(mid, (left + p) / 2) > 1)
+						{
+							spriteBatch.DrawLine(left, mid, color, thickness);
+							spriteBatch.DrawLine(mid, p, color, thickness);
+						}
+						else
+							spriteBatch.DrawLine(left, p, color, thickness);
+					}
+					if (y > 1)
+					{
+						up = ToVec2(points[x, y - 1].Position);
+						float thickness = x % 3 == 1 ? 3f : 1f;
+						int clampedY = Math.Min(y + 1, height - 1);
+						Vector2 mid = Vector2.CatmullRom(ToVec2(points[x, y - 2].Position), up, p, ToVec2(points[x, clampedY].Position), 0.5f);
+
+						if (Vector2.DistanceSquared(mid, (up + p) / 2) > 1)
+						{
+							spriteBatch.DrawLine(up, mid, color, thickness);
+							spriteBatch.DrawLine(mid, p, color, thickness);
+						}
+						else
+							spriteBatch.DrawLine(up, p, color, thickness);
+					}
+
+					// Add interpolated lines halfway between our point masses. This makes the grid look
+					// denser without the cost of simulating more springs and point masses.
+					if (x > 1 && y > 1)
+					{
+						Vector2 upLeft = ToVec2(points[x - 1, y - 1].Position);
+						spriteBatch.DrawLine(0.5f * (upLeft + up), 0.5f * (left + p), color, 1f);   // vertical line
+						spriteBatch.DrawLine(0.5f * (upLeft + left), 0.5f * (up + p), color, 1f);   // horizontal line
+					}
+				}
+			}
+		}
+
+		public void ApplyDirectedForce(Vector2 force, Vector2 position, float radius)
+		{
+			ApplyDirectedForce(new Vector3(force, 0), new Vector3(position, 0), radius);
+		}
+
+		public void ApplyDirectedForce(Vector3 force, Vector3 position, float radius)
+		{
+			foreach (var mass in points)
+				if (Vector3.DistanceSquared(position, mass.Position) < radius * radius)
+					mass.ApplyForce(10 * force / (10 + Vector3.Distance(position, mass.Position)));
+		}
+
+		public void ApplyImplosiveForce(float force, Vector2 position, float radius)
+		{
+			ApplyImplosiveForce(force, new Vector3(position, 0), radius);
+		}
+
+		public void ApplyImplosiveForce(float force, Vector3 position, float radius)
+		{
+			foreach (var mass in points)
+			{
+				float dist2 = Vector3.DistanceSquared(position, mass.Position);
+				if (dist2 < radius * radius)
+				{
+					mass.ApplyForce(10 * force * (position - mass.Position) / (100 + dist2));
+					mass.IncreaseDamping(0.6f);
+				}
+			}
+		}
+
+		public void ApplyExplosiveForce(float force, Vector2 position, float radius)
+		{
+			ApplyExplosiveForce(force, new Vector3(position, 0), radius);
+		}
+
+		public void ApplyExplosiveForce(float force, Vector3 position, float radius)
+		{
+			foreach (var mass in points)
+			{
+				float dist2 = Vector3.DistanceSquared(position, mass.Position);
+				if (dist2 < radius * radius)
+				{
+					mass.ApplyForce(100 * force * (mass.Position - position) / (10000 + dist2));
+					mass.IncreaseDamping(0.6f);
+				}
+			}
+		}
+
+		public Vector2 ToVec2(Vector3 v)
+		{
+			// do a perspective projection
+			float factor = (v.Z + 2000) / 2000;
+			return (new Vector2(v.X, v.Y) - screenSize / 2f) * factor + screenSize / 2;
+		}
 	}
 }
--- NeonShooter/NeonShooter/NeonShooter.Core/Utils/Extensions.cs
+++ NeonShooter/NeonShooter/NeonShooter.Core/Utils/Extensions.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using NeonShooter.Core.Graphics;
 using System;
 
 namespace NeonShooter.Core.Utils
@@ -25,6 +27,12 @@ namespace NeonShooter.Core.Utils
 			double theta = rand.NextDouble() * 2 * Math.PI;
 			float length = rand.NextFloat(minLength, maxLength);
 			return new Vector2(length * (float)Math.Cos(theta), length * (float)Math.Sin(theta));
+		}
+
+		public static void DrawLine(this SpriteBatch spriteBatch, Vector2 start, Vector2 end, Color color, float thickness = 2f)
+		{
+			Vector2 delta = end - start;
+			spriteBatch.Draw(Art.Pixel, start, null, color, delta.ToAngle(), new Vector2(0, 0.5f), new Vector2(delta.Length(), thickness), SpriteEffects.None, 0f);
 		}
 	}
 }
