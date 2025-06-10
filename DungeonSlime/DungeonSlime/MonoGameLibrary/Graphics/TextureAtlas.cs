0a195bf7aab1a33dd7afd621dcfa01d6bb47646c
JakeLegendXIII <jakelegendxiii@gmail.com>
2025-06-09 22:11:24 -0400

    Add TextureAtlas

---
+++ DungeonSlime/DungeonSlime/MonoGameLibrary/Graphics/TextureAtlas.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MonoGameLibrary.Graphics;
+
+public class TextureAtlas
+{
+    private Dictionary<string, TextureRegion> _regions;
+
+    /// <summary>
+    /// Gets or Sets the source texture represented by this texture atlas.
+    /// </summary>
+    public Texture2D Texture { get; set; }
+
+    /// <summary>
+    /// Creates a new texture atlas.
+    /// </summary>
+    public TextureAtlas()
+    {
+        _regions = new Dictionary<string, TextureRegion>();
+    }
+
+    /// <summary>
+    /// Creates a new texture atlas instance using the given texture.
+    /// </summary>
+    /// <param name="texture">The source texture represented by the texture atlas.</param>
+    public TextureAtlas(Texture2D texture)
+    {
+        Texture = texture;
+        _regions = new Dictionary<string, TextureRegion>();
+    }
+
+}
