7ab61c74d2c5445a7e126128e5352de702743c6c
JakeLegendXIII <jakelegendxiii@gmail.com>
2025-06-09 22:25:29 -0400

    First pass

--- DungeonSlime/DungeonSlime/DungeonSlime/Game1.cs
+++ DungeonSlime/DungeonSlime/DungeonSlime/Game1.cs
@@ -2,13 +2,17 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGameLibrary;
+using MonoGameLibrary.Graphics;
 
 namespace DungeonSlime;
 
 public class Game1 : Core
 {
-    // The MonoGame logo texture
-    private Texture2D _logo;
+    // texture region that defines the slime sprite in the atlas.
+    private TextureRegion _slime;
+
+    // texture region that defines the bat sprite in the atlas.
+    private TextureRegion _bat;
 
     public Game1() : base("Dungeon Slime", 1280, 720, false)
     {
@@ -24,7 +28,23 @@ public class Game1 : Core
 
     protected override void LoadContent()
     {
-        _logo = Content.Load<Texture2D>("Sprites/logo");
+        // Load the atlas texture using the content manager
+        Texture2D atlasTexture = Content.Load<Texture2D>("Sprites/atlas");
+
+        //  Create a TextureAtlas instance from the atlas
+        TextureAtlas atlas = new TextureAtlas(atlasTexture);
+
+        // add the slime region to the atlas.
+        atlas.AddRegion("slime", 0, 0, 20, 20);
+
+        // add the bat region to the atlas.
+        atlas.AddRegion("bat", 20, 0, 20, 20);
+
+        // retrieve the slime region from the atlas.
+        _slime = atlas.GetRegion("slime");
+
+        // retrieve the bat region from the atlas.
+        _bat = atlas.GetRegion("bat");
     }
 
     protected override void Update(GameTime gameTime)
@@ -42,48 +62,14 @@ public class Game1 : Core
         // Clear the back buffer.
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // The bounds of the icon within the texture.
-        Rectangle iconSourceRect = new Rectangle(0, 0, 128, 128);
-
-        // The bounds of the word mark within the texture.
-        Rectangle wordmarkSourceRect = new Rectangle(150, 34, 458, 58);
-
         // Begin the sprite batch to prepare for rendering.
-        SpriteBatch.Begin(sortMode: SpriteSortMode.FrontToBack);
+        SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
 
-        // Draw only the icon portion of the texture.
-        SpriteBatch.Draw(
-            _logo,              // texture
-            new Vector2(        // position
-                Window.ClientBounds.Width,
-                Window.ClientBounds.Height) * 0.5f,
-            iconSourceRect,     // sourceRectangle
-            Color.White,        // color
-            0.0f,               // rotation
-            new Vector2(        // origin
-                iconSourceRect.Width,
-                iconSourceRect.Height) * 0.5f,
-            1.0f,               // scale
-            SpriteEffects.None, // effects
-            1.0f                // layerDepth
-        );
+        // Draw the slime texture region at a scale of 4.0
+        _slime.Draw(SpriteBatch, Vector2.Zero, Color.White, 0.0f, Vector2.One, 4.0f, SpriteEffects.None, 0.0f);
 
-        // Draw only the word mark portion of the texture.
-        SpriteBatch.Draw(
-            _logo,              // texture
-            new Vector2(        // position
-              Window.ClientBounds.Width,
-              Window.ClientBounds.Height) * 0.5f,
-            wordmarkSourceRect, // sourceRectangle
-            Color.White,        // color
-            0.0f,               // rotation
-            new Vector2(        // origin
-              wordmarkSourceRect.Width,
-              wordmarkSourceRect.Height) * 0.5f,
-            1.0f,               // scale
-            SpriteEffects.None, // effects
-            0.0f                // layerDepth
-        );
+        // Draw the bat texture region 10px to the right of the slime at a scale of 4.0
+        _bat.Draw(SpriteBatch, new Vector2(_slime.Width * 4.0f + 10, 0), Color.White, 0.0f, Vector2.One, 4.0f, SpriteEffects.None, 1.0f);
 
         // Always end the sprite batch when finished.
         SpriteBatch.End();
