0ae5131a11952a52a544767633fae19e1fc96447
JakeLegendXIII <jakelegendxiii@gmail.com>
2025-06-09 21:39:29 -0400

    Green

--- DungeonSlime/DungeonSlime/DungeonSlime/Game1.cs
+++ DungeonSlime/DungeonSlime/DungeonSlime/Game1.cs
@@ -46,20 +46,19 @@ public class Game1 : Core
 
         // Draw the logo texture
         SpriteBatch.Draw(
-            _logo,                              // texture
-            new Vector2(                        // position
+            _logo,              // texture
+            new Vector2(        // position
                 Window.ClientBounds.Width,
                 Window.ClientBounds.Height) * 0.5f,
-            null,                               // sourceRectangle
-            Color.White,                        // color
-            0.0f,                               // rotation
-            new Vector2(                        // origin
+            null,               // sourceRectangle
+            Color.Green,        // color
+            0.0f,               // rotation
+            new Vector2(        // origin
                 _logo.Width,
                 _logo.Height) * 0.5f,
-            1.0f,                               // scale
-            SpriteEffects.FlipHorizontally |    // effects
-            SpriteEffects.FlipVertically,
-            0.0f                                // layerDepth
+            1.0f,               // scale
+            SpriteEffects.None, // effects
+            0.0f
         );
 
         // Always end the sprite batch when finished.
