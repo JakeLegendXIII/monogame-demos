09eed00e4be3366e3d72f026d96bfcb1a7ca8bdd
JakeLegendXIII <jakelegendxiii@gmail.com>
2025-06-09 21:34:29 -0400

    SpriteEffects demo

--- DungeonSlime/DungeonSlime/DungeonSlime/Game1.cs
+++ DungeonSlime/DungeonSlime/DungeonSlime/Game1.cs
@@ -46,19 +46,19 @@ public class Game1 : Core
 
         // Draw the logo texture
         SpriteBatch.Draw(
-            _logo,                      // texture
-            new Vector2(                // position
+            _logo,                          // texture
+            new Vector2(                    // position
                 Window.ClientBounds.Width,
                 Window.ClientBounds.Height) * 0.5f,
-            null,                       // sourceRectangle
-            Color.White,                // color
-            MathHelper.ToRadians(90),   // rotation
-            new Vector2(                // origin
+            null,                           // sourceRectangle
+            Color.White,                    // color
+            0.0f,                           // rotation
+            new Vector2(                    // origin
                 _logo.Width,
                 _logo.Height) * 0.5f,
-            1.0f,                       // scale
-            SpriteEffects.None,         // effects
-            0.0f                        // layerDepth
+            1.0f,                           // scale
+            SpriteEffects.FlipHorizontally, // effects
+            0.0f                            // layerDepth
         );
 
         // Always end the sprite batch when finished.
