154c7deb70fe18f8fd44663a16df529fe844c001
JakeLegendXIII <jakelegendxiii@gmail.com>
2025-06-11 21:14:40 -0400

    Start setting up Animation

---
+++ DungeonSlime/DungeonSlime/MonoGameLibrary/Graphics/Animation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MonoGameLibrary.Graphics;
+
+public class Animation
+{
+    /// <summary>
+    /// The texture regions that make up the frames of this animation.  The order of the regions within the collection
+    /// are the order that the frames should be displayed in.
+    /// </summary>
+    public List<TextureRegion> Frames { get; set; }
+
+    /// <summary>
+    /// The amount of time to delay between each frame before moving to the next frame for this animation.
+    /// </summary>
+    public TimeSpan Delay { get; set; }
+
+}
