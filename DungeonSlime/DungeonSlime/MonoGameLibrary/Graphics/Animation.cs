14181b9f9d3f3f668ea25636ded1b23545da2166
JakeLegendXIII <jakelegendxiii@gmail.com>
2025-06-11 21:20:11 -0400

    Start settting up animation data

--- DungeonSlime/DungeonSlime/MonoGameLibrary/Graphics/Animation.cs
+++ DungeonSlime/DungeonSlime/MonoGameLibrary/Graphics/Animation.cs
@@ -15,5 +15,25 @@ public class Animation
     /// The amount of time to delay between each frame before moving to the next frame for this animation.
     /// </summary>
     public TimeSpan Delay { get; set; }
+    
+    /// <summary>
+    /// Creates a new animation.
+    /// </summary>
+    public Animation()
+    {
+        Frames = new List<TextureRegion>();
+        Delay = TimeSpan.FromMilliseconds(100);
+    }
+
+    /// <summary>
+    /// Creates a new animation with the specified frames and delay.
+    /// </summary>
+    /// <param name="frames">An ordered collection of the frames for this animation.</param>
+    /// <param name="delay">The amount of time to delay between each frame of this animation.</param>
+    public Animation(List<TextureRegion> frames, TimeSpan delay)
+    {
+        Frames = frames;
+        Delay = delay;
+    }
 
 }
