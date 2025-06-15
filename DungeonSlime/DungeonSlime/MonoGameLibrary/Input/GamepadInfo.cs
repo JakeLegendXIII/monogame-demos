0a0a03a2bc49775b19dc38680eff7c8a0e475613
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-06-15 12:51:48 -0400

    Start creating GamepadInfo

---
+++ DungeonSlime/DungeonSlime/MonoGameLibrary/Input/GamepadInfo.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace MonoGameLibrary.Input;
+
+public class GamePadInfo
+{
+	private TimeSpan _vibrationTimeRemaining = TimeSpan.Zero;
+
+	/// <summary>
+	/// Gets the index of the player this gamepad is for.
+	/// </summary>
+	public PlayerIndex PlayerIndex { get; }
+
+	/// <summary>
+	/// Gets the state of input for this gamepad during the previous update cycle.
+	/// </summary>
+	public GamePadState PreviousState { get; private set; }
+
+	/// <summary>
+	/// Gets the state of input for this gamepad during the current update cycle.
+	/// </summary>
+	public GamePadState CurrentState { get; private set; }
+
+	/// <summary>
+	/// Gets a value that indicates if this gamepad is currently connected.
+	/// </summary>
+	public bool IsConnected => CurrentState.IsConnected;
+
+
+}
