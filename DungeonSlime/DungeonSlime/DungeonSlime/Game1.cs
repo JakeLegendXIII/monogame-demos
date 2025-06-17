bc85d2408a965d410d4fad7259ea0f8a7f1e37eb
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-06-16 22:45:17 -0400

    Add Collision Detection

--- DungeonSlime/DungeonSlime/DungeonSlime/Game1.cs
+++ DungeonSlime/DungeonSlime/DungeonSlime/Game1.cs
@@ -10,52 +10,65 @@ namespace DungeonSlime;
 
 public class Game1 : Core
 {
-    // Defines the slime animated sprite.
-    private AnimatedSprite _slime;
-    // Defines the bat animated sprite.
-    private AnimatedSprite _bat;
+	// Defines the slime animated sprite.
+	private AnimatedSprite _slime;
+
+	// Defines the bat animated sprite.
+	private AnimatedSprite _bat;
+
 	// Tracks the position of the slime.
 	private Vector2 _slimePosition;
 
 	// Speed multiplier when moving.
 	private const float MOVEMENT_SPEED = 5.0f;
 
+	// Tracks the position of the bat.
+	private Vector2 _batPosition;
+
+	// Tracks the velocity of the bat.
+	private Vector2 _batVelocity;
+
 	public Game1() : base("Dungeon Slime", 1280, 720, false)
-    {
+	{
 
-    }
+	}
 
-    protected override void Initialize()
-    {
-        // TODO: Add your initialization logic here
+	protected override void Initialize()
+	{
+		base.Initialize();
 
-        base.Initialize();
-    }
+		// Set the initial position of the bat to be 10px
+		// to the right of the slime.
+		_batPosition = new Vector2(_slime.Width + 10, 0);
 
-    protected override void LoadContent()
-    {
-        // Create the texture atlas from the XML configuration file
-        TextureAtlas atlas = TextureAtlas.FromFile(Content, "Sprites/atlas-definition.xml");
+		// Assign the initial random velocity to the bat.
+		AssignRandomBatVelocity();
+	}
 
-        // Create the slime animated sprite from the atlas.
-        _slime = atlas.CreateAnimatedSprite("slime-animation");
-        _slime.Scale = new Vector2(4.0f, 4.0f);
+	protected override void LoadContent()
+	{
+		// Create the texture atlas from the XML configuration file
+		TextureAtlas atlas = TextureAtlas.FromFile(Content, "Sprites/atlas-definition.xml");
 
-        // Create the bat animated sprite from the atlas.
-        _bat = atlas.CreateAnimatedSprite("bat-animation");
-        _bat.Scale = new Vector2(4.0f, 4.0f);
-    }
+		// Create the slime animated sprite from the atlas.
+		_slime = atlas.CreateAnimatedSprite("slime-animation");
+		_slime.Scale = new Vector2(4.0f, 4.0f);
 
-    protected override void Update(GameTime gameTime)
-    {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-            Exit();
+		// Create the bat animated sprite from the atlas.
+		_bat = atlas.CreateAnimatedSprite("bat-animation");
+		_bat.Scale = new Vector2(4.0f, 4.0f);
+	}
 
-        // Update the slime animated sprite
-        _slime.Update(gameTime);
+	protected override void Update(GameTime gameTime)
+	{
+		if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+			Exit();
 
-        // Update the bat animated sprite
-        _bat.Update(gameTime);
+		// Update the slime animated sprite.
+		_slime.Update(gameTime);
+
+		// Update the bat animated sprite.
+		_bat.Update(gameTime);
 
 		// Check for keyboard input and handle it.
 		CheckKeyboardInput();
@@ -63,8 +76,124 @@ public class Game1 : Core
 		// Check for gamepad input and handle it.
 		CheckGamePadInput();
 
+		// Create a bounding rectangle for the screen
+		Rectangle screenBounds = new Rectangle(
+			0,
+			0,
+			GraphicsDevice.PresentationParameters.BackBufferWidth,
+			GraphicsDevice.PresentationParameters.BackBufferHeight
+		);
+
+		// Creating a bounding circle for the slime
+		Circle slimeBounds = new Circle(
+			(int)(_slimePosition.X + (_slime.Width * 0.5f)),
+			(int)(_slimePosition.Y + (_slime.Height * 0.5f)),
+			(int)(_slime.Width * 0.5f)
+		);
+
+		// Use distance based checks to determine if the slime is within the
+		// bounds of the game screen, and if it is outside that screen edge,
+		// move it back inside.
+		if (slimeBounds.Left < screenBounds.Left)
+		{
+			_slimePosition.X = screenBounds.Left;
+		}
+		else if (slimeBounds.Right > screenBounds.Right)
+		{
+			_slimePosition.X = screenBounds.Right - _slime.Width;
+		}
+
+		if (slimeBounds.Top < screenBounds.Top)
+		{
+			_slimePosition.Y = screenBounds.Top;
+		}
+		else if (slimeBounds.Bottom > screenBounds.Bottom)
+		{
+			_slimePosition.Y = screenBounds.Bottom - _slime.Height;
+		}
+
+		// Calculate the new position of the bat based on the velocity
+		Vector2 newBatPosition = _batPosition + _batVelocity;
+
+		// Create a bounding circle for the bat
+		Circle batBounds = new Circle(
+			(int)(newBatPosition.X + (_bat.Width * 0.5f)),
+			(int)(newBatPosition.Y + (_bat.Height * 0.5f)),
+			(int)(_bat.Width * 0.5f)
+		);
+
+		Vector2 normal = Vector2.Zero;
+
+		// Use distance based checks to determine if the bat is within the
+		// bounds of the game screen, and if it is outside that screen edge,
+		// reflect it about the screen edge normal
+		if (batBounds.Left < screenBounds.Left)
+		{
+			normal.X = Vector2.UnitX.X;
+			newBatPosition.X = screenBounds.Left;
+		}
+		else if (batBounds.Right > screenBounds.Right)
+		{
+			normal.X = -Vector2.UnitX.X;
+			newBatPosition.X = screenBounds.Right - _bat.Width;
+		}
+
+		if (batBounds.Top < screenBounds.Top)
+		{
+			normal.Y = Vector2.UnitY.Y;
+			newBatPosition.Y = screenBounds.Top;
+		}
+		else if (batBounds.Bottom > screenBounds.Bottom)
+		{
+			normal.Y = -Vector2.UnitY.Y;
+			newBatPosition.Y = screenBounds.Bottom - _bat.Height;
+		}
+
+		// If the normal is anything but Vector2.Zero, this means the bat had
+		// moved outside the screen edge so we should reflect it about the
+		// normal.
+		if (normal != Vector2.Zero)
+		{
+			_batVelocity = Vector2.Reflect(_batVelocity, normal);
+		}
+
+		_batPosition = newBatPosition;
+
+		if (slimeBounds.Intersects(batBounds))
+		{
+			// Divide the width  and height of the screen into equal columns and
+			// rows based on the width and height of the bat.
+			int totalColumns = GraphicsDevice.PresentationParameters.BackBufferWidth / (int)_bat.Width;
+			int totalRows = GraphicsDevice.PresentationParameters.BackBufferHeight / (int)_bat.Height;
+
+			// Choose a random row and column based on the total number of each
+			int column = Random.Shared.Next(0, totalColumns);
+			int row = Random.Shared.Next(0, totalRows);
+
+			// Change the bat position by setting the x and y values equal to
+			// the column and row multiplied by the width and height.
+			_batPosition = new Vector2(column * _bat.Width, row * _bat.Height);
+
+			// Assign a new random velocity to the bat
+			AssignRandomBatVelocity();
+		}
+
 		base.Update(gameTime);
-    }
+	}
+
+	private void AssignRandomBatVelocity()
+	{
+		// Generate a random angle
+		float angle = (float)(Random.Shared.NextDouble() * Math.PI * 2);
+
+		// Convert angle to a direction vector
+		float x = (float)Math.Cos(angle);
+		float y = (float)Math.Sin(angle);
+		Vector2 direction = new Vector2(x, y);
+
+		// Multiply the direction vector by the movement speed
+		_batVelocity = direction * MOVEMENT_SPEED;
+	}
 
 	private void CheckKeyboardInput()
 	{
@@ -110,11 +239,11 @@ public class Game1 : Core
 		if (gamePadOne.IsButtonDown(Buttons.A))
 		{
 			speed *= 1.5f;
-			gamePadOne.SetVibration(1.0f, TimeSpan.FromSeconds(1));
+			GamePad.SetVibration(PlayerIndex.One, 1.0f, 1.0f);
 		}
 		else
 		{
-			gamePadOne.StopVibration();
+			GamePad.SetVibration(PlayerIndex.One, 0.0f, 0.0f);
 		}
 
 		// Check thumbstick first since it has priority over which gamepad input
@@ -154,23 +283,22 @@ public class Game1 : Core
 	}
 
 	protected override void Draw(GameTime gameTime)
-    {
-        // Clear the back buffer.
-        GraphicsDevice.Clear(Color.CornflowerBlue);
+	{
+		// Clear the back buffer.
+		GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // Begin the sprite batch to prepare for rendering.
-        SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+		// Begin the sprite batch to prepare for rendering.
+		SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
 
 		// Draw the slime sprite.
-		_slime.Draw(SpriteBatch, _slimePosition);		
+		_slime.Draw(SpriteBatch, _slimePosition);
 
-		// Draw the bat texture region 10px to the right of the slime at a scale of 4.0
-		_bat.Draw(SpriteBatch, new Vector2(_slime.Width + 10, 0));
+		// Draw the bat sprite.
+		_bat.Draw(SpriteBatch, _batPosition);
 
-        // Always end the sprite batch when finished.
-        SpriteBatch.End();
+		// Always end the sprite batch when finished.
+		SpriteBatch.End();
 
-        base.Draw(gameTime);
-    }
-
+		base.Draw(gameTime);
+	}
 }
