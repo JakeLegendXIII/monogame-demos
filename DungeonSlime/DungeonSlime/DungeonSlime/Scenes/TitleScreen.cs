337e8c35327623cfca01b9e314ad0c5e4607dec1
JakeLegendXIII <jakelegendxiii@gmail.com>
2025-06-29 19:49:39 -0400

    Start creating Scenes structure in game

---
+++ DungeonSlime/DungeonSlime/DungeonSlime/Scenes/TitleScreen.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGameLibrary;
+using MonoGameLibrary.Scenes;
+
+namespace DungeonSlime.Scenes;
+
+public class TitleScene : Scene
+{
+
+}
