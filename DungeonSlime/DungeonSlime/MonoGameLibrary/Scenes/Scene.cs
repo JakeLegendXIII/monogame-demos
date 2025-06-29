c7087afb9ceebe01ebef41bd44540a8e1fe66aa3
JakeLegendXIII <jakelegendxiii@gmail.com>
2025-06-29 19:17:37 -0400

    Create Abstract base class for scenes

---
+++ DungeonSlime/DungeonSlime/MonoGameLibrary/Scenes/Scene.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace MonoGameLibrary.Scenes;
+
+public abstract class Scene : IDisposable
+{
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
