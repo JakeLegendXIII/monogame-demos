47eee01c85ad98b5a5b73d7d6d44df6c297f3be4
JakeLegendXIII <jakelegendxiii@gmail.com>
2025-06-30 20:27:35 -0400

    Setting it up

--- DungeonSlime/DungeonSlime/DungeonSlime/Scenes/GameScene.cs
+++ DungeonSlime/DungeonSlime/DungeonSlime/Scenes/GameScene.cs
@@ -12,5 +12,84 @@ namespace DungeonSlime.Scenes;
 
 public class GameScene : Scene
 {
+    // Defines the slime animated sprite.
+    private AnimatedSprite _slime;
+
+    // Defines the bat animated sprite.
+    private AnimatedSprite _bat;
+
+    // Tracks the position of the slime.
+    private Vector2 _slimePosition;
+
+    // Speed multiplier when moving.
+    private const float MOVEMENT_SPEED = 5.0f;
+
+    // Tracks the position of the bat.
+    private Vector2 _batPosition;
+
+    // Tracks the velocity of the bat.
+    private Vector2 _batVelocity;
+
+    // Defines the tilemap to draw.
+    private Tilemap _tilemap;
+
+    // Defines the bounds of the room that the slime and bat are contained within.
+    private Rectangle _roomBounds;
+
+    // The sound effect to play when the bat bounces off the edge of the screen.
+    private SoundEffect _bounceSoundEffect;
+
+    // The sound effect to play when the slime eats a bat.
+    private SoundEffect _collectSoundEffect;
+
+    // The SpriteFont Description used to draw text
+    private SpriteFont _font;
+
+    // Tracks the players score.
+    private int _score;
+
+    // Defines the position to draw the score text at.
+    private Vector2 _scoreTextPosition;
+
+    // Defines the origin used when drawing the score text.
+    private Vector2 _scoreTextOrigin;
+
+    public override void Initialize()
+    {
+        // LoadContent is called during base.Initialize().
+        base.Initialize();
+
+        // During the game scene, we want to disable exit on escape. Instead,
+        // the escape key will be used to return back to the title screen
+        Core.ExitOnEscape = false;
+
+        Rectangle screenBounds = Core.GraphicsDevice.PresentationParameters.Bounds;
+
+        _roomBounds = new Rectangle(
+             (int)_tilemap.TileWidth,
+             (int)_tilemap.TileHeight,
+             screenBounds.Width - (int)_tilemap.TileWidth * 2,
+             screenBounds.Height - (int)_tilemap.TileHeight * 2
+         );
+
+        // Initial slime position will be the center tile of the tile map.
+        int centerRow = _tilemap.Rows / 2;
+        int centerColumn = _tilemap.Columns / 2;
+        _slimePosition = new Vector2(centerColumn * _tilemap.TileWidth, centerRow * _tilemap.TileHeight);
+
+        // Initial bat position will the in the top left corner of the room.
+        _batPosition = new Vector2(_roomBounds.Left, _roomBounds.Top);
+
+        // Set the position of the score text to align to the left edge of the
+        // room bounds, and to vertically be at the center of the first tile.
+        _scoreTextPosition = new Vector2(_roomBounds.Left, _tilemap.TileHeight * 0.5f);
+
+        // Set the origin of the text so it is left-centered.
+        float scoreTextYOrigin = _font.MeasureString("Score").Y * 0.5f;
+        _scoreTextOrigin = new Vector2(0, scoreTextYOrigin);
+
+        // Assign the initial random velocity to the bat.
+        AssignRandomBatVelocity();
+    }
 
 }
