27fb5e261d17802e7d8f1ec9679c31a67c92c3f5
JakeLegendXIII <jakelegendxiii@gmail.com>
2025-06-30 20:22:08 -0400

    Start building TitleScreen

--- DungeonSlime/DungeonSlime/DungeonSlime/Scenes/TitleScreen.cs
+++ DungeonSlime/DungeonSlime/DungeonSlime/Scenes/TitleScreen.cs
@@ -8,5 +8,66 @@ namespace DungeonSlime.Scenes;
 
 public class TitleScene : Scene
 {
+    private const string DUNGEON_TEXT = "Dungeon";
+    private const string SLIME_TEXT = "Slime";
+    private const string PRESS_ENTER_TEXT = "Press Enter To Start";
+
+    // The font to use to render normal text.
+    private SpriteFont _font;
+
+    // The font used to render the title text.
+    private SpriteFont _font5x;
+
+    // The position to draw the dungeon text at.
+    private Vector2 _dungeonTextPos;
+
+    // The origin to set for the dungeon text.
+    private Vector2 _dungeonTextOrigin;
+
+    // The position to draw the slime text at.
+    private Vector2 _slimeTextPos;
+
+    // The origin to set for the slime text.
+    private Vector2 _slimeTextOrigin;
+
+    // The position to draw the press enter text at.
+    private Vector2 _pressEnterPos;
+
+    // The origin to set for the press enter text when drawing it.
+    private Vector2 _pressEnterOrigin;
+
+    public override void Initialize()
+    {
+        // LoadContent is called during base.Initialize().
+        base.Initialize();
+
+        // While on the title screen, we can enable exit on escape so the player
+        // can close the game by pressing the escape key.
+        Core.ExitOnEscape = true;
+
+        // Set the position and origin for the Dungeon text.
+        Vector2 size = _font5x.MeasureString(DUNGEON_TEXT);
+        _dungeonTextPos = new Vector2(640, 100);
+        _dungeonTextOrigin = size * 0.5f;
+
+        // Set the position and origin for the Slime text.
+        size = _font5x.MeasureString(SLIME_TEXT);
+        _slimeTextPos = new Vector2(757, 207);
+        _slimeTextOrigin = size * 0.5f;
+
+        // Set the position and origin for the press enter text.
+        size = _font.MeasureString(PRESS_ENTER_TEXT);
+        _pressEnterPos = new Vector2(640, 620);
+        _pressEnterOrigin = size * 0.5f;
+    }
+
+    public override void LoadContent()
+    {
+        // Load the font for the standard text.
+        _font = Core.Content.Load<SpriteFont>("fonts/04B_30");
+
+        // Load the font for the title text.
+        _font5x = Content.Load<SpriteFont>("fonts/04B_30_5x");
+    }
 
 }
