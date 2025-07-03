a2999566ca65bec4973184bc3ff49d9a3fa73136
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-07-02 23:34:09 -0400

    Update TitleScreen background

--- DungeonSlime/DungeonSlime/DungeonSlime/Scenes/TitleScreen.cs
+++ DungeonSlime/DungeonSlime/DungeonSlime/Scenes/TitleScreen.cs
@@ -35,8 +35,21 @@ public class TitleScene : Scene
 
     // The origin to set for the press enter text when drawing it.
     private Vector2 _pressEnterOrigin;
+     
+	// The texture used for the background pattern.
+	private Texture2D _backgroundPattern;
 
-    public override void Initialize()
+	// The destination rectangle for the background pattern to fill.
+	private Rectangle _backgroundDestination;
+
+	// The offset to apply when drawing the background pattern so it appears to
+	// be scrolling.
+	private Vector2 _backgroundOffset;
+
+	// The speed that the background pattern scrolls.
+	private float _scrollSpeed = 50.0f;
+
+	public override void Initialize()
     {
         // LoadContent is called during base.Initialize().
         base.Initialize();
@@ -59,7 +72,14 @@ public class TitleScene : Scene
         size = _font.MeasureString(PRESS_ENTER_TEXT);
         _pressEnterPos = new Vector2(640, 620);
         _pressEnterOrigin = size * 0.5f;
-    }
+
+		// Initialize the offset of the background pattern at zero.
+		_backgroundOffset = Vector2.Zero;
+
+		// Set the background pattern destination rectangle to fill the entire
+		// screen background.
+		_backgroundDestination = Core.GraphicsDevice.PresentationParameters.Bounds;
+	}
 
     public override void LoadContent()
     {
@@ -68,7 +88,10 @@ public class TitleScene : Scene
 
         // Load the font for the title text.
         _font5x = Content.Load<SpriteFont>("fonts/04B_30_5x");
-    }
+
+		// Load the background pattern texture.
+		_backgroundPattern = Content.Load<Texture2D>("Sprites/background-pattern");
+	}
 
     public override void Update(GameTime gameTime)
     {
@@ -77,11 +100,27 @@ public class TitleScene : Scene
         {
             Core.ChangeScene(new GameScene());
         }
-    }
+
+		// Update the offsets for the background pattern wrapping so that it
+		// scrolls down and to the right.
+		float offset = _scrollSpeed * (float)gameTime.ElapsedGameTime.TotalSeconds;
+		_backgroundOffset.X -= offset;
+		_backgroundOffset.Y -= offset;
+
+		// Ensure that the offsets do not go beyond the texture bounds so it is
+		// a seamless wrap.
+		_backgroundOffset.X %= _backgroundPattern.Width;
+		_backgroundOffset.Y %= _backgroundPattern.Height;
+	}
 
     public override void Draw(GameTime gameTime)
     {
-        Core.GraphicsDevice.Clear(new Color(32, 40, 78, 255));
+		Core.GraphicsDevice.Clear(new Color(32, 40, 78, 255));
+
+		// Draw the background pattern first using the PointWrap sampler state.
+		Core.SpriteBatch.Begin(samplerState: SamplerState.PointWrap);
+		Core.SpriteBatch.Draw(_backgroundPattern, _backgroundDestination, new Rectangle(_backgroundOffset.ToPoint(), _backgroundDestination.Size), Color.White * 0.5f);
+		Core.SpriteBatch.End();		
 
         // Begin the sprite batch to prepare for rendering.
         Core.SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
