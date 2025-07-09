cd44b62ebef9657d18e4f329fb132c57047dd923
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-07-08 21:57:29 -0400

    Update GameScene

--- DungeonSlime/DungeonSlime/DungeonSlime/Scenes/GameScene.cs
+++ DungeonSlime/DungeonSlime/DungeonSlime/Scenes/GameScene.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using DungeonSlime.UI;
 using Gum.DataTypes;
+using Gum.Managers;
 using Gum.Wireframe;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
@@ -12,6 +14,7 @@ using MonoGameLibrary;
 using MonoGameLibrary.Graphics;
 using MonoGameLibrary.Input;
 using MonoGameLibrary.Scenes;
+
 
 namespace DungeonSlime.Scenes;
 
@@ -65,10 +68,14 @@ public class GameScene : Scene
 
 	// A reference to the resume button UI element so we can focus it
 	// when the game is paused.
-	private Button _resumeButton;
+	private AnimatedButton _resumeButton;
 
 	// The UI sound effect to play when a UI event is triggered.
 	private SoundEffect _uiSoundEffect;
+
+	// Reference to the texture atlas that we can pass to UI elements when they
+	// are created.
+	private TextureAtlas _atlas;
 
 
 	public override void Initialize()
@@ -113,16 +120,16 @@ public class GameScene : Scene
 
     public override void LoadContent()
     {
-        // Create the texture atlas from the XML configuration file.
-        TextureAtlas atlas = TextureAtlas.FromFile(Core.Content, "Sprites/atlas-definition.xml");
+		// Create the texture atlas from the XML configuration file
+		_atlas = TextureAtlas.FromFile(Core.Content, "Sprites/atlas-definition.xml");
 
-        // Create the slime animated sprite from the atlas.
-        _slime = atlas.CreateAnimatedSprite("slime-animation");
-        _slime.Scale = new Vector2(4.0f, 4.0f);
+		// Create the slime animated sprite from the atlas.
+		_slime = _atlas.CreateAnimatedSprite("slime-animation");
+		_slime.Scale = new Vector2(4.0f, 4.0f);
 
-        // Create the bat animated sprite from the atlas.
-        _bat = atlas.CreateAnimatedSprite("bat-animation");
-        _bat.Scale = new Vector2(4.0f, 4.0f);
+		// Create the bat animated sprite from the atlas.
+		_bat = _atlas.CreateAnimatedSprite("bat-animation");
+		_bat.Scale = new Vector2(4.0f, 4.0f);
 
         // Create the tilemap from the XML configuration file.
         _tilemap = Tilemap.FromFile(Content, "Sprites/tilemap-definition.xml");
@@ -464,36 +471,45 @@ public class GameScene : Scene
 		_pausePanel.IsVisible = false;
 		_pausePanel.AddToRoot();
 
-		var background = new ColoredRectangleRuntime();
+		TextureRegion backgroundRegion = _atlas.GetRegion("panel-background");
+
+		NineSliceRuntime background = new NineSliceRuntime();
 		background.Dock(Dock.Fill);
-		background.Color = Color.DarkBlue;
+		background.Texture = backgroundRegion.Texture;
+		background.TextureAddress = TextureAddress.Custom;
+		background.TextureHeight = backgroundRegion.Height;
+		background.TextureLeft = backgroundRegion.SourceRectangle.Left;
+		background.TextureTop = backgroundRegion.SourceRectangle.Top;
+		background.TextureWidth = backgroundRegion.Width;
 		_pausePanel.AddChild(background);
 
-		var textInstance = new TextRuntime();
+		TextRuntime textInstance = new TextRuntime();
 		textInstance.Text = "PAUSED";
+		textInstance.CustomFontFile = @"Fonts/04b_30.fnt";
+		textInstance.UseCustomFont = true;
+		textInstance.FontScale = 0.5f;
 		textInstance.X = 10f;
 		textInstance.Y = 10f;
 		_pausePanel.AddChild(textInstance);
 
-		_resumeButton = new Button();
+		_resumeButton = new AnimatedButton(_atlas);
 		_resumeButton.Text = "RESUME";
 		_resumeButton.Anchor(Anchor.BottomLeft);
 		_resumeButton.Visual.X = 9f;
 		_resumeButton.Visual.Y = -9f;
-		_resumeButton.Visual.Width = 80;
 		_resumeButton.Click += HandleResumeButtonClicked;
 		_pausePanel.AddChild(_resumeButton);
 
-		var quitButton = new Button();
+		AnimatedButton quitButton = new AnimatedButton(_atlas);
 		quitButton.Text = "QUIT";
 		quitButton.Anchor(Anchor.BottomRight);
 		quitButton.Visual.X = -9f;
 		quitButton.Visual.Y = -9f;
-		quitButton.Width = 80;
 		quitButton.Click += HandleQuitButtonClicked;
 
 		_pausePanel.AddChild(quitButton);
 	}
+
 
 	private void HandleResumeButtonClicked(object sender, EventArgs e)
 	{
