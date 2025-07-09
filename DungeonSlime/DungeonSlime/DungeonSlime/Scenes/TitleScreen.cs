159765fcafbbe1bdb41c309ac4b865583da902e3
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-07-08 21:40:48 -0400

    Updated TitleScreen

--- DungeonSlime/DungeonSlime/DungeonSlime/Scenes/TitleScreen.cs
+++ DungeonSlime/DungeonSlime/DungeonSlime/Scenes/TitleScreen.cs
@@ -254,13 +254,18 @@ public class TitleScene : Scene
 		_optionsPanel.IsVisible = false;
 		_optionsPanel.AddToRoot();
 
-		var optionsText = new TextRuntime();
+		TextRuntime optionsText = new TextRuntime();
 		optionsText.X = 10;
 		optionsText.Y = 10;
 		optionsText.Text = "OPTIONS";
+		optionsText.UseCustomFont = true;
+		optionsText.FontScale = 0.5f;
+		optionsText.CustomFontFile = @"fonts/04b_30.fnt";
 		_optionsPanel.AddChild(optionsText);
 
-		var musicSlider = new Slider();
+		OptionsSlider musicSlider = new OptionsSlider(_atlas);
+		musicSlider.Name = "MusicSlider";
+		musicSlider.Text = "MUSIC";
 		musicSlider.Anchor(Gum.Wireframe.Anchor.Top);
 		musicSlider.Visual.Y = 30f;
 		musicSlider.Minimum = 0;
@@ -272,7 +277,9 @@ public class TitleScene : Scene
 		musicSlider.ValueChangeCompleted += HandleMusicSliderValueChangeCompleted;
 		_optionsPanel.AddChild(musicSlider);
 
-		var sfxSlider = new Slider();
+		OptionsSlider sfxSlider = new OptionsSlider(_atlas);
+		sfxSlider.Name = "SfxSlider";
+		sfxSlider.Text = "SFX";
 		sfxSlider.Anchor(Gum.Wireframe.Anchor.Top);
 		sfxSlider.Visual.Y = 93;
 		sfxSlider.Minimum = 0;
@@ -284,7 +291,7 @@ public class TitleScene : Scene
 		sfxSlider.ValueChangeCompleted += HandleSfxSliderChangeCompleted;
 		_optionsPanel.AddChild(sfxSlider);
 
-		_optionsBackButton = new Button();
+		_optionsBackButton = new AnimatedButton(_atlas);
 		_optionsBackButton.Text = "BACK";
 		_optionsBackButton.Anchor(Gum.Wireframe.Anchor.BottomRight);
 		_optionsBackButton.X = -28f;
@@ -292,6 +299,7 @@ public class TitleScene : Scene
 		_optionsBackButton.Click += HandleOptionsButtonBack;
 		_optionsPanel.AddChild(_optionsBackButton);
 	}
+
 
 	private void HandleSfxSliderChanged(object sender, EventArgs args)
 	{
