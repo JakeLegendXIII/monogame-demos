9a5a931bd65883451f92f79850b69d1727eebb55
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-07-15 21:31:53 -0400

    Start setting stuff up to use shader

--- DungeonSlime/DungeonSlime/DungeonSlime/Scenes/GameScene.cs
+++ DungeonSlime/DungeonSlime/DungeonSlime/Scenes/GameScene.cs
@@ -42,6 +42,16 @@ public class GameScene : Scene
 
 	private GameState _state;
 
+	// The grayscale shader effect.
+	private Effect _grayscaleEffect;
+
+	// The amount of saturation to provide the grayscale shader effect.
+	private float _saturation = 1.0f;
+
+	// The speed of the fade to grayscale effect.
+	private const float FADE_SPEED = 0.02f;
+
+
 	public override void Initialize()
 	{
 		// LoadContent is called during base.Initialize().
