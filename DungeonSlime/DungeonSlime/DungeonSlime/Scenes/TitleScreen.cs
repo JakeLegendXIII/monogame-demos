bacf01d12dc6d10912eb331c2c5ff00770ae4f26
Jake Kirkbride <jakelegendxiii@gmail.com>
2025-07-31 20:27:14 -0400

    Small thing

--- DungeonSlime/DungeonSlime/DungeonSlime/Scenes/TitleScreen.cs
+++ DungeonSlime/DungeonSlime/DungeonSlime/Scenes/TitleScreen.cs
@@ -3,7 +3,6 @@ using DungeonSlime.UI;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
 using MonoGameGum;
 using MonoGameGum.Forms.Controls;
 using MonoGameGum.GueDeriving;
